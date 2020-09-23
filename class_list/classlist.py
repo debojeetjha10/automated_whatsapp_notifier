39d673ff9e34a3aee1c19dacce9ab4c408af0724
Debojeet Jha <debojeetjha@gmail.com>
2020-09-23 15:47:06 +0530

    added class list :)

---
+++ class_list/classlist.py
@@ -0,0 +1,16 @@
+monday = [{'type': "lab", 'start_hour': 9,'start_mint':00, 'end_hour': 11,'end_mint':50,'class_info': "data structure lab by IB sir-for hx link :https://meet.google.com/iez-ezgj-xnz DLCD lab by PG sir-for hy class code : it-iiest-ug3-dcld-lab "},
+            {'type': 'Normal', 'start_hour': 12,'start_mint':00, 'end_hour': 12,'end_mint':50, 'class_info': "Discreat Mathamatics class by CG sir link : https://meet.google.com/ank-xxvm-mfa "},
+            {'type':'Normal','start_hour':14,'start_mint':30,'end_hour': 16,'end_mint':20,'class_info':"DLCD class by PG sir join via class code : it-iiest-ug3-dlcd "}]
+tuesday = [{'type':'Normal','start_hour':9,'start_mint':00,'end_hour': 10,'end_mint':50,'class_info':"Data structure class by IB sir . class link : https://meet.google.com/tbs-axtg-ogv "},
+			{'type':'Normal','start_hour':11,'start_mint':00,'end_hour': 12,'end_mint':50,'class_info':"Mathematics Class . class link  : https://meet.google.com/gik-vxfw-use "},
+			{'type':'lab','start_hour':14,'start_mint':30,'end_hour': 17,'end_mint':20,'class_info':"DLCD lab by PG sir for hx . class code :  it-iiest-ug3-dlcd-lab .Programming lab by SD sir for Hy . Class link : https://meet.google.com/tjp-byqo-gzm "}]
+
+wednesday = [{'type':'Normal','start_hour':10,'start_mint':00,'end_hour': 10,'end_mint':50,'class_info':"Signals and system class by spm sir. class link : https://meet.google.com/ciy-zwye-xbo "},
+				{'type':'Normal','start_hour':11,'start_mint':00,'end_hour': 12,'end_mint':50,'class_info':"Mathematics class, The link is atached in the google calender just login into meet and join the class "}]
+thursday = [{'type':'lab','start_hour':10,'start_mint':00,'end_hour': 12,'end_mint':50,'class_info':"Programming lab by SD sir-for Hx . class link : https://meet.google.com/jky-jqik-zov . Data Structure lab by CG sir-For Hy . Class link : https://meet.google.com/yaw-wdch-azg "},
+			{'type':'Normal','start_hour':14,'start_mint':30,'end_hour': 16,'end_mint':20,'class_info':"DLCD class by PG sir join via class code : it-iiest-ug3-dlcd "}]
+friday = [{'type': 'Normal', 'start_hour': 9,'start_mint':00, 'end_hour': 10,'end_mint':50, 'class_info': "Discreat Mathamatics class by CG sir . Class link : https://meet.google.com/hhk-qaax-uzj "},
+			{'type': 'Normal', 'start_hour': 11,'start_mint':00, 'end_hour': 12,'end_mint':50, 'class_info': "Signals and system class by SPM sir . Class link : https://meet.google.com/pnt-zcah-pfv "},
+			{'type': 'Normal', 'start_hour': 14,'start_mint':30, 'end_hour': 16,'end_mint':20, 'class_info': "Data structure class by Ib sir . Class link : https://meet.google.com/dbv-atrc-ufo "}]
+day = friday
+message = f"you have a {day[0]['type']} class starting from {day[0]['start_hour']}:{day[0]['start_mint']} .The class info is : {day[0]['class_info']}"
